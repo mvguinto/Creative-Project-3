9ff9d864f336f126de99adb7e0037d3a90778e5d
Derek Hensley <hensley.derek58@gmail.com>
2020-03-10 15:13:34 -0600

    Added new link to random
    Clicking on random brings up a random recipe from the database.

--- creative_project_3/src/App.vue
+++ creative_project_3/src/App.vue
@@ -1,39 +1,40 @@
 <template>
 <div id="app">
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/Browse">Browse</router-link>
-  </div>
-  <router-view />
+	<div id="nav">
+		<router-link to="/">Home</router-link> |
+		<router-link to="/Browse">Browse</router-link> |
+		<router-link :to="{ name: 'Recipe', params: {recipeName: 'random' }}">Random</router-link>
+	</div>
+	<router-view />
 </div>
 </template>
 
 <style>
 body {
-  background: #80808C;
+	background: #80808C;
 }
 
 #app {
-  font-family: 'Indie Flower', cursive;
-  font-size: 25px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin: 40px;
-  padding: 20px 25%;
-  background: #FFFFFF;
+	font-family: 'Indie Flower', cursive;
+	font-size: 25px;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	margin: 40px;
+	padding: 20px 25%;
+	background: #FFFFFF;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
