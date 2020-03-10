e76d6213b2ad359746e6a474e81b44718c5d7ba2
Maleah Guinto <maleah.guinto@gmail.com>
2020-03-10 15:46:33 -0500

    Added CSS and image to Home view
    Added some css to format the home view as well as changing to vue logo
    to the chef logo.`

--- creative_project_3/src/views/Home.vue
+++ creative_project_3/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+  <img alt="Chef logo" src="../../public/images/Chef-Logo.jpg">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</div>
 </template>
 
 <script>
