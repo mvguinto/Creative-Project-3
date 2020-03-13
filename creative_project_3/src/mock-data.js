1aaf6d2fb85b494a869b6db46670a0af225d6064
Derek Hensley <hensley.derek58@gmail.com>
2020-03-13 12:37:32 -0600

    Finished mock data
    Added all the recipes to the mock data.

--- creative_project_3/src/mock-data.js
+++ creative_project_3/src/mock-data.js
@@ -337,7 +337,458 @@ let mock = [{
     difficulty: "Easy",
     imageSource: "https://www.flickr.com/photos/potaufeu/385459112/in/photolist-A4zyL-7TUwS9-87ktzo-FKV7nh-dRX3pL-xsny6-4xUvys-5NQtxb-aq75sJ-7Et9zq-7avtLp-9Uw3x6-8HD8cL-7e3zJa-8HD88C-6cS9Qr-492RAs-8FWQU-6HEcov-8FWPb-9jPmeY-4tThN7-4RsxUm-i18QvG-5VuKvE-77Vjj5-B5qv4-8FnTr5-6JXC4Y-3f9BZq-9EdHNj-cTsHW-4qFJDV-kdjsXc-cTsHU-dvXY6M-7rXo87-48XS9F-9Z7r1L-8AUSsT-97CLuk-8HD86E-2wAUS-8AXYFq-5yfhzx-492PTY-2VkT1h-5BZ1bp-gMi7q5-8HD841",
     recipeSource: "https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/",
-  },
+  }, {
+    id: 9,
+    name: "Adobo",
+    image: 'Adobo.jpg',
+    ingredients: [{
+        name: "white vinegar",
+        measurement: "1 cup"
+      },
+      {
+        name: "soy sauce",
+        measurement: "1/4 cup"
+      },
+      {
+        name: "garlic",
+        measurement: "1 bulb"
+      },
+      {
+        name: "salt",
+        measurement: "2 teaspoons"
+      },
+      {
+        name: "ground pepper",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "bay leaf",
+        measurement: "1"
+      },
+      {
+        name: "chicken thighs or drumsticks",
+        measurement: "2 pounds"
+      },
+      {
+        name: "canola oil",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "water",
+        measurement: "1 cup"
+      }
+    ],
+    recipe: [
+      "Combine the first six ingredients. Add chicken; refrigerate, covered, 20-30 minutes. Drain, reserving marinade. Pat chicken dry.",
+      "In a large skillet, heat oil over medium-high heat; brown chicken. Stir in water and reserved marinade. Bring to a boil. Reduce heat; simmer, uncovered, until chicken is no longer pink and sauce is slightly reduced, 20-25 minutes. If desired, serve chicken with cooking sauce."
+    ],
+    difficulty: "Hard",
+  }, {
+    id: 10,
+    name: "Lasagna",
+    image: 'Lasagna.jpg',
+    ingredients: [{
+        name: "ground beef",
+        measurement: "1 pound"
+      },
+      {
+        name: "diced onion",
+        measurement: "3/4 cups"
+      },
+      {
+        name: "diced bell pepper (green, red, or yellow)",
+        measurement: "3/4 cups"
+      },
+      {
+        name: "tomato sauce",
+        measurement: "28 ounces"
+      },
+      {
+        name: "tomato paste",
+        measurement: "3 ounces"
+      },
+      {
+        name: "crushed tomatoes",
+        measurement: "14 ounces"
+      },
+      {
+        name: "oregano",
+        measurement: "2 tablespoons (fresh) or 2 teaspoons (dried)"
+      },
+      {
+        name: "parsley",
+        measurement: "1/4 cup"
+      },
+      {
+        name: "Italian Seasoning",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "garlic salt or galic powder",
+        measurement: ""
+      },
+      {
+        name: "red or white wine vinegar",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "salt",
+        measurement: ""
+      },
+      {
+        name: "dry lasagna noodles",
+        measurement: "1/2 pound"
+      },
+      {
+        name: "Ricotta cheese",
+        measurement: "15 ounces"
+      },
+      {
+        name: "Mozarella cheese",
+        measurement: "1 1/2 pounds"
+      },
+      {
+        name: "Parmesan cheese",
+        measurement: "1/4 pounds"
+      }
+    ],
+    recipe: [
+      "Put pasta water on to boil: Put a large pot of salted water (1 Tbsp salt for every 2 quarts of water) on the stovetop on high heat. It can take a while for a large pot of water to come to a boil (this will be your pasta water), so prepare the sauce in the next steps while the water is heating.",
+      "Brown the ground beef: In a large skillet heat 2 teaspoons of olive oil on medium high heat. Add the ground beef and cook until it is lightly browned on all sides. Remove the beef with a slotted spoon to a bowl. Drain off all but a tablespoon of fat.",
+      "Cook the bell pepper, onions, garlic, add back the beef: Add the diced bell pepper and onions to skillet (in the photo we are using yellow bell pepper and red onions). Cook for 4 to 5 minutes, until the onions are translucent and the peppers softened. Add the minced garlic and cook half a minute more. Return the browned ground beef to the pan. Stir to combine, reduce the heat to low and cook for another 5 minutes.",
+      "Transfer to medium sized pot, add tomatoes and remaining sauce ingredients to build the sauce: Transfer the beef mixture to a medium sized (3- to 4-quart) pot. Add the crushed tomatoes, tomato sauce, and tomato paste to the pot. Add the parsley, oregano, and Italian seasonings, adjusting the amounts to taste. Sprinkle with garlic powder and/or garlic salt, to taste. Sprinkle with red or white wine vinegar. Stir in sugar, a tablespoon at a time, tasting after each addition, to taste. (The amount of sugar needed will vary, depending on how acidic the tomatoes are that you are using.) Add salt to taste, and note that you will later be adding Parmesan, which is salty. Bring the sauce to a simmer and then lower the heat to maintain a low simmer.  Cook for 15 to 45 minutes, stirring often. Scrape the bottom of the pot every so often so nothing sticks to the bottom and scorches. Remove from heat.",
+      "Boil and drain the lasagna noodles: By now the salted water you started heating in step one should be boiling. Add the dry lasagna noodles and cook them to al dente, per package directions. (Note noodles may be cooked in advance.) Stir often to prevent from sticking. Make sure that water remains at a full rolling boil during the entire cooking to prevent noodles from sticking. When ready, drain in a colander and rinse with cool water, gently separating any noodles that may be sticking together. Spread a little olive oil on a large rimmed baking sheet, and lay out the cooked noodles on this sheet, turning them over so that they get coated with a little of the olive oil. This will prevent them from sticking together.",
+      "Heat the oven to 375°F",
+      "Assemble the lasagna: In a 9x13-inch casserole or lasagna dish, ladle a cup of sauce and spread it over the bottom of the dish. Arrange one layer of lasagna noodles lengthwise (about 3 long noodles, edge may overlap, depending on your pan) over the sauce. Ladle a third of the remaining sauce over the noodles. Sprinkle a layer of a third of the grated mozzarella on top of lasagna sauce. Add half of the ricotta cheese, by placing cheese dollops every couple inches. Sprinkle half the grated parmesan cheese evenly over the top of ricotta cheese. Apply second layer of noodles, top it with half of the remaining sauce. Add half of the remaining Mozzarella, the remaining ricotta cheese, and another the remaining Parmesan. Finish with another layer of noodles. Spread the remaining sauce over the top layer of noodles and sprinkle with the remaining Mozzarella cheese.",
+      "Bake: Cover the lasagna pan with aluminum foil, tented slightly so it doesn't touch the noodles or sauce). Bake at 375°F for 45 minutes. Uncover in the last 10 minutes if you'd like more of a crusty top or edges.",
+      "Cool and serve: Allow the lasagna to cool at least 15 before serving. Leftovers will keep for about 5 days. May be reheated in conventional oven or microwave. Leave aluminum tent on for storage. (Try to keep the aluminum foil from touching the sauce.)"
+    ],
+    difficulty: "Very Hard",
+    recipeSource: "https://www.simplyrecipes.com/recipes/lasagna/",
+    imageSource: "https://www.simplyrecipes.com/recipes/lasagna/"
+  }, {
+    id: 11,
+    name: "Fried Potatoes",
+    image: 'Potatoes.jpg',
+    ingredients: [{
+        name: "baby potatoes",
+        measurement: "1 pound"
+      },
+      {
+        name: "vegetable oil",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "extra-virign olive oil",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "freshly chopped rosemary",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "garlic powder",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "chili powder",
+        measurement: "1/2 teaspoon"
+      },
+      {
+        name: "salt",
+        measurement: ""
+      },
+      {
+        name: "pepper",
+        measurement: ""
+      }
+    ],
+    recipe: [
+      "Slice potatoes into coins about ¼” thick. In a large skillet over medium-high heat, heat oils. Add potatoes and season with rosemary, salt and pepper. Cook, undisturbed, until potatoes are golden and crusty underneath, 4 to 5 minutes. Flip potatoes and cook until golden on other sides, 4 to 5 minutes more.",
+      "Sprinkle with garlic powder and chili powder, and continue to cook, stirring occasionally, until potatoes are tender, about 2 minutes more. Serve warm."
+    ],
+    difficulty: "Easy",
+    recipeSource: "https://www.delish.com/cooking/a23463225/pan-fried-potatoes/",
+    imageSource: "https://www.delish.com/cooking/a23463225/pan-fried-potatoes/"
+  }, {
+    id: 12,
+    name: "Fajitas",
+    image: 'Fajitas.jpg',
+    ingredients: [{
+        name: "chili powder",
+        measurement: "2 teaspoons"
+      },
+      {
+        name: "ground cumin",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "smoked paprika",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "minced garlic",
+        measurement: "3 cloves"
+      },
+      {
+        name: "flat iron steak",
+        measurement: "1 1/2 pounds"
+      },
+      {
+        name: "conola oil",
+        measurement: "2 1/2 tablespoons"
+      },
+      {
+        name: "red bell pepper",
+        measurement: "1"
+      },
+      {
+        name: "yellow bell pepper",
+        measurement: "1"
+      },
+      {
+        name: "red onion",
+        measurement: "1"
+      },
+      {
+        name: "salt",
+        measurement: ""
+      },
+      {
+        name: "pepper",
+        measurement: ""
+      }
+    ],
+    recipe: [
+      "Preheat grill to medium high heat.",
+      "In a small bowl, combine chili powder, cumin, paprika, garlic, 1 teaspoon salt and 1/2 teaspoon pepper. Season steak with chili powder mixture.",
+      "Brush steak with 1 tablespoon canola oil. Add steak to grill, and cook, flipping once, until desired doneness, about 4-5 minutes per side for medium rare. Let rest 5 minutes.",
+      "Season bell peppers and onion with remaining 1 1/2 tablespoons canola oil; season with salt and pepper, to taste.",
+      "Add bell peppers and onion to grill, and cook, turning occasionally, until charred and tender, about 6-8 minutes.",
+      "Thinly slice steak against the grain and serve with bell peppers and onion."
+    ],
+    difficulty: "Easy",
+    recipeSource: "https://damndelicious.net/2019/10/24/easy-steak-fajitas//",
+    imageSource: "https://damndelicious.net/2019/10/24/easy-steak-fajitas/"
+  }, {
+    id: 13,
+    name: "Sisig",
+    image: 'Sisig.jpg',
+    ingredients: [{
+        name: "pig ears",
+        measuresent: "1 pound"
+      },
+      {
+        name: "pork belly",
+        measurement: "1 1/2 pound"
+      },
+      {
+        name: "onion",
+        measurement: "1"
+      },
+      {
+        name: "soy sauce",
+        measurement: "3 tablespoons"
+      },
+      {
+        name: "ground black pepper",
+        measurement: "1/4 teaspoon"
+      },
+      {
+        name: "minced ginger",
+        measurement: "1 knob"
+      },
+      {
+        name: "chili flakes",
+        measurement: "3 tablespoons"
+      },
+      {
+        name: "garlic powder",
+        measurement: "1/2 teaspoon"
+      },
+      {
+        name: "lemon",
+        measurement: "1"
+      },
+      {
+        name: "butter",
+        measurement: "1/2 cup"
+      },
+      {
+        name: "chicken liver",
+        measurement: "1/3 pound"
+      },
+      {
+        name: "water",
+        measurement: "6 cups"
+      },
+      {
+        name: "mayonnaise",
+        measurement: "3 tablespoons"
+      },
+      {
+        name: "salt",
+        measurement: "1/2 teaspoon"
+      }
+    ],
+    recipe: [
+      "Pour the water in a pan and bring to a boil. Add salt and pepper.",
+      "Put-in the pig’s ears and pork belly then simmer for 40 minutes to 1 hour (or until tender).",
+      "Remove the boiled ingredients from the pot then drain excess water",
+      "Grill the boiled pig ears and pork belly until done",
+      "Chop the pig ears and pork belly into fine pieces",
+      "In a wide pan, melt the butter or margarine. Add the onions. Cook until onions are soft.",
+      "Put-in the ginger and cook for 2 minutes",
+      "Add the chicken liver. Crush the chicken liver while cooking it in the pan.",
+      "Add the chopped pig ears and pork belly. Cook for 10 to 12 minutes",
+      "Put-in the soy sauce, garlic powder, and chili. Mix well",
+      "Add salt and pepper to taste",
+      "Put-in the mayonnaise and mix with the other ingredients",
+      "Transfer to a serving plate. Top with chopped green onions and raw egg.",
+      "Serve hot. Share and Enjoy (add the lemon or calamansi before eating)"
+    ],
+    difficulty: "Very Hard",
+    recipeSource: "https://panlasangpinoy.com/pork-sisig/",
+    imageSource: "https://panlasangpinoy.com/pork-sisig/"
+  }, {
+    id: 14,
+    name: "Pinakbet",
+    image: 'Pinakbet.jpg',
+    ingredients: [{
+        name: "olive oil",
+        measuresent: "3 tablespoons"
+      },
+      {
+        name: "onion",
+        measurement: "1"
+      },
+      {
+        name: "minced garlic",
+        measurement: "2 cloves"
+      },
+      {
+        name: "pork loin",
+        measurement: "1/2 pound"
+      },
+      {
+        name: "prawns",
+        measurement: "1/2 pound"
+      },
+      {
+        name: "tomato",
+        measurement: "1"
+      },
+      {
+        name: "zucchini",
+        measurement: "1/4 pound"
+      },
+      {
+        name: "okra",
+        measurement: "1/4 pound"
+      },
+      {
+        name: "green beans",
+        measurement: "1/4 pound"
+      },
+      {
+        name: "eggplant",
+        measurement: "1/4 pound"
+      },
+      {
+        name: "bitter melon",
+        measurement: "1 small"
+      },
+      {
+        name: "salt",
+        measurement: ""
+      },
+      {
+        name: "pepper",
+        measurement: ""
+      }
+    ],
+    recipe: [
+      "Heat the oil in a large pot over medium heat; cook and stir the onion and garlic in the hot oil until tender, about 5 minutes. Stir in the pork and cook until completely browned, 5 to 7 minutes. Stir the shrimp into the mixture; season with salt and pepper. Continue cooking until the shrimp turn pink, about 5 minutes. Add the tomato to the pot, cover, and let cook 5 minutes. Stir the zucchini, okra, green beans, eggplant, and bitter melon into the mixture; cover, and cook until the vegetables are all soft to the touch, about 10 minutes more. Serve hot."
+    ],
+    difficulty: "Hard",
+    recipeSource: "https://www.allrecipes.com/recipe/212994/pinakbet/",
+    imageSource: "https://www.allrecipes.com/recipe/212994/pinakbet/"
+  }, {
+    id: 15,
+    name: "Chicken Curry",
+    image: 'Curry.jpg',
+    ingredients: [{
+        name: "chicken thighs",
+        measuresent: "2 pounds"
+      },
+      {
+        name: "onion",
+        measurement: "1"
+      },
+      {
+        name: "minced garlic",
+        measurement: "4 cloves"
+      },
+      {
+        name: "Fresh broccoli florets",
+        measurement: "1 cup"
+      },
+      {
+        name: "Fresh cauliflower florets",
+        measurement: "1 cup"
+      },
+      {
+        name: "chopped carrots",
+        measurement: "1 cup"
+      },
+      {
+        name: "tomato paste",
+        measurement: "3 tablespoons"
+      },
+      {
+        name: "grated fresh ginger",
+        measurement: "1 tablespoon"
+      },
+      {
+        name: "curry powder",
+        measurement: "2 teaspoons"
+      },
+      {
+        name: "cajun seasoning",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "italian seasoning",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "chicken broth",
+        measurement: "1/2 cup"
+      },
+      {
+        name: "coconut milk",
+        measurement: "2 cups"
+      },
+      {
+        name: "lime juice",
+        measurement: "1/4 cup"
+      },
+      {
+        name: "salt",
+        measurement: ""
+      },
+      {
+        name: "pepper",
+        measurement: ""
+      }
+    ],
+    recipe: [
+      "Heat the oil in a large pot over medium heat; cook and stir the onion and garlic in the hot oil until tender, about 5 minutes. Stir in the pork and cook until completely browned, 5 to 7 minutes. Stir the shrimp into the mixture; season with salt and pepper. Continue cooking until the shrimp turn pink, about 5 minutes. Add the tomato to the pot, cover, and let cook 5 minutes. Stir the zucchini, okra, green beans, eggplant, and bitter melon into the mixture; cover, and cook until the vegetables are all soft to the touch, about 10 minutes more. Serve hot."
+    ],
+    difficulty: "Easy",
+    recipeSource: "http://wholeandheavenlyoven.com/2015/03/12/30-minute-chicken-veggie-curry-2/",
+    imageSource: "http://wholeandheavenlyoven.com/2015/03/12/30-minute-chicken-veggie-curry-2/"
+  }
 ]
 
 export default mock;
