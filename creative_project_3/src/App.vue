126ccd92438d3dc7d426cffdc9a57aa2c7cf3151
Maleah Guinto <maleah.guinto@gmail.com>
2020-03-12 19:32:59 -0500

    Completed Recipe css and updated recipes
    Completed the formating and css for the Recipe page and updated the
    mock data with correct information

--- creative_project_3/src/App.vue
+++ creative_project_3/src/App.vue
@@ -1,63 +1,63 @@
 <template>
 <div id="app">
-	<div id="nav">
-		<router-link to="/">Home</router-link> |
-		<router-link to="/Browse">Browse</router-link> |
-		<router-link :to="{ name: 'Recipe', params: {recipeName: 'random' }}">Random</router-link>
-	</div>
-	<div class="content-wrapper">
-		<router-view />
-	</div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Browse">Browse</router-link> |
+    <router-link :to="{ name: 'Recipe', params: {recipeName: 'random' }}">Random</router-link>
+  </div>
+  <div class="content-wrapper">
+    <router-view />
+  </div>
 </div>
 </template>
 
 <style>
 body {
-	background: #321e17;
-	padding: 5%;
+  background: #321e17;
+  padding: 5%;
 }
 
 #app {
-	font-family: 'Indie Flower', cursive;
-	font-size: 15px;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	background: #FFFFFF;
+  font-family: 'Indie Flower', cursive;
+  font-size: 15px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: #FFFFFF;
 }
 
 .content-wrapper {
-	margin: 5%;
-	padding: 0% 15% 10% 15%;
+  margin: 5%;
+  padding: 0% 15% 10% 15%;
 }
 
 #nav {
-	padding: 10% 7.5%;
-	background: #FFFFFF url("/images/Rolling-Pin.jpg") no-repeat center;
-	background-size: 100%;
+  padding: 10% 7.5%;
+  background: #FFFFFF url("/images/Rolling-Pin.jpg") no-repeat center;
+  background-size: 100%;
 }
 
 #nav a {
-	font-weight: bold;
-	color: #f2ebc9;
+  font-weight: bold;
+  color: #f2ebc9;
 }
 
 #nav a.router-link-exact-active {
-	color: #321e17;
+  color: #321e17;
 }
 
 @media only screen and (min-width: 961px) {
-	#app {
-		font-size: 25px;
-	}
+  #app {
+    font-size: 35px;
+  }
 
-	#nav {
-		background-size: 75%
-	}
+  #nav {
+    background-size: 75%
+  }
 
-	.content-wrapper {
-		margin: 0%;
-		padding: 0% 15% 10% 15%;
-	}
+  .content-wrapper {
+    margin: 0%;
+    padding: 0% 15% 3% 15%;
+  }
 }
 </style>
--- creative_project_3/src/mock-data.js
+++ creative_project_3/src/mock-data.js
@@ -1,117 +1,101 @@
 let mock = [{
     id: 1,
-    name: "Bread",
-    country: "United States",
+    name: "Italian Bread",
     image: 'Bread.jpg',
     ingredients: [{
         name: "active dry yeast",
-        measurement: "1 package"
-      },
-      {
-        name: "warm water",
-        measurement: "2-1/4 cups"
-      },
-      {
-        name: "sugar",
-        measurement: "3 tablespoons and 1/2 teaspoon"
-      },
-      {
-        name: "salt",
-        measurement: "1 tablespoon"
-      },
-      {
-        name: "canola oil",
-        measurement: "2 tablespoons"
+        measurement: "1 (1/4-ounce) envelope"
       },
       {
         name: "bread flour",
-        measurement: "6-1/4 to 6-3/4 cups"
+        measurement: "2 to 3 cups"
+      },
+      {
+        name: "sugar",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "salt",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "olive oil",
+        measurement: "2 tablespoons"
+      },
+      {
+        name: "warm water (100° to 110°)",
+        measurement: "1 cup"
       }
     ],
     recipe: [
-      "In a large bowl, dissolve yeast and 1/2 teaspoon sugar in warm water; let stand until bubbles form on surface. Whisk together remaining 3 tablespoons sugar, salt, and 3 cups flour. Stir oil into yeast mixture; pour into flour mixture and beat until smooth. Stir in enough remaining flour, 1/2 cup at a time, to form a soft dough.",
-      "Turn onto a floured surface; knead until smooth and elastic, 8-10 minutes. Place in a greased bowl, turning once to grease the top. Cover and let rise in a warm place until doubled, 1-1/2 to 2 hours.",
-      "Punch dough down. Turn onto a lightly floured surface; divide dough in half. Shape each into a loaf. Place in 2 greased 9x5-in. loaf pans. Cover and let rise until doubled, 1 to 1-1/2 hours.",
-      "Bake at 375° until golden brown and bread sounds hollow when tapped or has reached an internal temperature of 200°, 30-35 minutes. Remove from pans to wire racks to cool."
+      "Combine yeast, sugar, and 1 cup warm water in bowl of a heavy-duty electric stand mixer; let stand 5 minutes. Add 2 cups flour, oil, and salt to bowl, and beat at low speed, using dough hook attachment, 1 minute. Gradually add additional flour until dough begins to leave the sides of the bowl and pull together. (Note: The dough will take on a 'shaggy' appearance as the flour is being added. When enough flour has been added, the dough will look soft and smooth, not wet and sticky or overly dry with a rough surface.)",
+      "Increase speed to medium, and beat 5 minutes. Cover bowl of dough with plastic wrap, and let stand in a warm place (85°), free from drafts, 30 minutes or until doubled in bulk. Punch dough down, and let stand 10 minutes.",
+      "Turn dough out onto a lightly floured surface; shape dough into a 12-inch loaf, and place on a lightly greased baking sheet. Cut 3 (1/4-inch deep) slits across top of dough with a sharp paring knife. (The slits release interior steam and prevent the loaf from blowing apart at the side.)",
+      "Bake at 400° for 16 minutes or until golden brown. Cool on a wire rack."
     ],
     difficulty: "Hard",
+    imageSource: "https://www.flickr.com/photos/cuttingboard/3952783930/in/photolist-72i3X5-Tds8yG-9ZgTkh-9z4r8f-a61Ryz-7nC1a9-rWNjU-dEuqWt-bA7QsV-qKy4WF-bwcmuf-4esUNi-4gEyUw-4KG1bq-id84EC-8HU4r2-9qbRyT-aqSiAc-o2FfMj-RQJ1gP-72i3Uj-7W8iT7-5dafJZ-fP8AMj-23w3xZx-sJAig-bxoRdF-8qUdaa-9wHXgD-aqUWUf-91wC1Q-5rJNqL-9WBb8U-cjcHs3-dkk8Lr-4Lgb9v-9z6myp-7wX6Cz-aqSjg4-d8SPc5-7D1Ztr-eQA3Zk-Q2B9nS-ADK3i-id7G4H-55qWrT-aqUZCy-aD8Cgm-cRDdh-qUQKtE",
+    recipeSource: "https://www.myrecipes.com/recipe/italian-bread",
   },
   {
     id: 2,
-    name: "Cookies",
-    country: "United States",
+    name: "Oreo White Chocolate Pudding Cookies",
     image: 'Cookies.jpg',
     ingredients: [{
-        name: "active dry yeast",
-        measurement: "1 package"
+        name: "room temperature, unsalted butter",
+        measurement: "1 cup"
       },
       {
-        name: "warm water",
-        measurement: "2-1/4 cups"
+        name: "brown sugar",
+        measurement: "3/4 cup"
       },
       {
-        name: "sugar",
-        measurement: "3 tablespoons and 1/2 teaspoon"
+        name: "granulated sugar",
+        measurement: "1/4 cup"
+      },
+      {
+        name: "white chocolate instant pudding mix",
+        measurement: "1 3.4 oz package"
+      },
+      {
+        name: "eggs",
+        measurement: "2 large"
+      },
+      {
+        name: "vanilla extract",
+        measurement: "1 teaspoon"
+      },
+      {
+        name: "all-purpose flour",
+        measurement: "2 1/2 cups"
+      },
+      {
+        name: "baking soda",
+        measurement: "1 teaspoon"
       },
       {
         name: "salt",
-        measurement: "1 tablespoon"
+        measurement: "1/2 teaspoon"
       },
       {
-        name: "canola oil",
-        measurement: "2 tablespoons"
+        name: "chopped Oreos",
+        measurement: "2 cups"
       },
       {
-        name: "bread flour",
-        measurement: "6-1/4 to 6-3/4 cups"
+        name: "white chocolate chips",
+        measurement: "1 1/2 cups"
       }
     ],
     recipe: [
-      "In a large bowl, dissolve yeast and 1/2 teaspoon sugar in warm water; let stand until bubbles form on surface. Whisk together remaining 3 tablespoons sugar, salt, and 3 cups flour. Stir oil into yeast mixture; pour into flour mixture and beat until smooth. Stir in enough remaining flour, 1/2 cup at a time, to form a soft dough.",
-      "Turn onto a floured surface; knead until smooth and elastic, 8-10 minutes. Place in a greased bowl, turning once to grease the top. Cover and let rise in a warm place until doubled, 1-1/2 to 2 hours.",
-      "Punch dough down. Turn onto a lightly floured surface; divide dough in half. Shape each into a loaf. Place in 2 greased 9x5-in. loaf pans. Cover and let rise until doubled, 1 to 1-1/2 hours.",
-      "Bake at 375° until golden brown and bread sounds hollow when tapped or has reached an internal temperature of 200°, 30-35 minutes. Remove from pans to wire racks to cool."
+      "Preheat oven to 350 degrees F. Line a baking sheet with parchment paper or a Silpat baking mat and set aside.",
+      "Using a mixer, beat together butter and sugars until creamy. Add in white chocolate pudding mix, eggs, and vanilla extract. In a medium bowl, whisk together the flour, baking soda, and salt. Add the dry ingredients to the wet ingredients and mix until just combined. Stir in the Oreos and white chocolate chips.",
+      "Drop cookie dough by rounded tablespoons onto prepared baking sheet. Bake for 10 minutes, or until slightly golden around the edges and set. Remove cookies from oven and let cool on baking sheet for two minutes. Transfer to a cooling rack and cool completely.",
+      "Note-these cookies will stay soft for 3-4 days. Store in an air-tight container on the counter."
     ],
-    difficulty: "Very Hard",
+    difficulty: "Easy",
+    imageSource: "https://search.creativecommons.org/photos/38db1f38-5805-46f5-b56b-0c25f3914967",
+    recipeSource: "https://www.twopeasandtheirpod.com/oreo-white-chocolate-pudding-cookies/",
   },
-  {
-    id: 2,
-    name: "Cookies",
-    country: "United States",
-    image: 'Cookies.jpg',
-    ingredients: [{
-        name: "active dry yeast",
-        measurement: "1 package"
-      },
-      {
-        name: "warm water",
-        measurement: "2-1/4 cups"
-      },
-      {
-        name: "sugar",
-        measurement: "3 tablespoons and 1/2 teaspoon"
-      },
-      {
-        name: "salt",
-        measurement: "1 tablespoon"
-      },
-      {
-        name: "canola oil",
-        measurement: "2 tablespoons"
-      },
-      {
-        name: "bread flour",
-        measurement: "6-1/4 to 6-3/4 cups"
-      }
-    ],
-    recipe: [
-      "In a large bowl, dissolve yeast and 1/2 teaspoon sugar in warm water; let stand until bubbles form on surface. Whisk together remaining 3 tablespoons sugar, salt, and 3 cups flour. Stir oil into yeast mixture; pour into flour mixture and beat until smooth. Stir in enough remaining flour, 1/2 cup at a time, to form a soft dough.",
-      "Turn onto a floured surface; knead until smooth and elastic, 8-10 minutes. Place in a greased bowl, turning once to grease the top. Cover and let rise in a warm place until doubled, 1-1/2 to 2 hours.",
-      "Punch dough down. Turn onto a lightly floured surface; divide dough in half. Shape each into a loaf. Place in 2 greased 9x5-in. loaf pans. Cover and let rise until doubled, 1 to 1-1/2 hours.",
-      "Bake at 375° until golden brown and bread sounds hollow when tapped or has reached an internal temperature of 200°, 30-35 minutes. Remove from pans to wire racks to cool."
-    ],
-    difficulty: "Very Hard",
-  }
 ]
 
 export default mock;
